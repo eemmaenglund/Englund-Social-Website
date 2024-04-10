0f97c0d19d0769d8f50c86604f1a12f16de75d7f
Emma Englund <artbyemmae@gmail.com>
2024-04-09 18:17:20 -0700

    script comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 // jQuery shorthand for executing code when the DOM is fully loaded
 $(function(){
 
+    // Initializing parallax effect on image with id "img1" using image source
     $("#img1").parallax({
         imageSrc: "images/emma3.jpeg" 
     });
-
+    // Initializing parallax effect on image with id "img2" using specified image source
     $("#img2").parallax({
         imageSrc: "images/laptop.jpg" 
     });
 
+    // Initializing background video for element with class "banner-video"
     $(".banner-video").bgVideo({
-        pauseAfter: 0,
-        fadeIn: 0,
-        showPausePlay: false
+        pauseAfter: 0, // No pause after video finishes
+        fadeIn: 0, // No fade-in effect
+        showPausePlay: false // Hide pause/play button
     });
 
 });
