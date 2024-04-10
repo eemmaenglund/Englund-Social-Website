0f6d2cf880df71e2f1020ecd09b64ae3ec69fdf1
Emma Englund <artbyemmae@gmail.com>
2024-04-09 17:48:37 -0700

    Comments for GSAP

--- js/GSAP.js
+++ js/GSAP.js
@@ -1,23 +1,36 @@
+// Adding ScrollTrigger plugin for scroll animation sync
 gsap.registerPlugin(ScrollTrigger);
   
           gsap.utils.toArray('.midsection-wrap img').forEach((img, i) => {
+            // Selecting each image in the midsection and animating them
               gsap.from(img, {
+                // Applying animation properties to each image
                   scrollTrigger: {
+                    // Defining trigger element for scroll animation
                       trigger: img,
+                      // Setting the start point for animation trigger
                       start: 'top center+=100',
-                      toggleActions: 'play none none reverse',
+                      toggleActions: 'play none none reverse', // Playing animation when entering, reversing when leaving
                   },
+                  // Setting start opacity to 0
                   opacity: 0,
+                  // moving vertically by 50 pixels
                   y: 50,
+                   // animation duration
                   duration: 0.7,
+                  // Adding delay to each image's animation based on index
                   delay: i * 0.2,
               });
           });
 
+          // Re-registering plugin for further use
+
           gsap.registerPlugin(ScrollTrigger);
 
           gsap.utils.toArray('.image-container1 img').forEach((img, i) => {
-              gsap.from(img, {
+              // Selecting each image in the image container 1 and animating them
+             gsap.from(img, {
+                // rest is the same from above
                   scrollTrigger: {
                       trigger: img,
                       start: 'top center',
